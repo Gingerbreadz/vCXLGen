92b6a9f8f30ccac383e6a4c59a205022bf789f87
Anatole Lefort <anatole.lefort@gmail.com>
2024-10-30 16:20:24 +0100

    Murphi: reset proxy_msg vars when nested transaction is completed
        proxy_msg vars should be undefined as soon as possible to reduce state space size.
        it is safe to do so, after returning from a nested transaction (that succeeded)

--- Algorithms/ControllerGeneration/General/DeferMessage/NestedDeferMessage.py
+++ Algorithms/ControllerGeneration/General/DeferMessage/NestedDeferMessage.py
@@ -80,4 +80,8 @@ class NestedDeferMessage(BaseDeferMessage):
             proxy_key = self.__proxy_key
             if isinstance(cur_guard, BaseMessage):
                 proxy_key = self.__proxy_key_from_message(cur_guard)
-            transition.rename_operation(str(cur_guard), proxy_key)
+            mutated = transition.rename_operation(str(cur_guard), proxy_key)
+            if mutated:
+                undef_proxy_msg_node = CommonTree(CommonToken(text=ProtoParserBase.k_undef))
+                undef_proxy_msg_node.addChild(CommonTree(CommonToken(text=proxy_key)))
+                transition.operations.append(undef_proxy_msg_node)
