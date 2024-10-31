36573a2da6d7388dd4d86ae6be6815415da2b495
Anatole Lefort <anatole.lefort@gmail.com>
2024-10-31 12:59:26 +0100

    Murphi: useful comments for improvements

--- Backend/Murphi/MurphiModular/General/GenPCCToMurphi_DECOM.py
+++ Backend/Murphi/MurphiModular/General/GenPCCToMurphi_DECOM.py
@@ -281,6 +281,11 @@ class GenPCCtoMurphi_Rev(GenDataTypes, GenMurphiAccess, Debug):
                         break
 
             if not var_type:
+                # TODO Warning raised for .cl field access in RequestL1/RequestL2 msg types (PutS msg essentially)
+                #      Harmful, because it re-initializes cbe.cl on a PutS message
+                #      The issue exists in HeteroGen as well, most likely a ProtoGen bug
+                #      Fix can be done later, as a correct cache controller never sends PutS message when in M state.
+                #          This generates dead code essentially.
                 self.pwarning("The variable: " + obj_var + " is not defined for the message type " +
                               str(msg_defs[0].msg_type))
                 if obj_var in self.config.super_type_defs:
