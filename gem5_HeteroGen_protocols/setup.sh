55b24319308d5063e745d928b99dcd10a94938fa
Anatole Lefort <anatole.lefort@gmail.com>
2025-12-16 14:44:42 +0100

    update README

--- gem5_HeteroGen_protocols/setup.sh
+++
@@ -1,8 +0,0 @@
-# Remove old scons files
-rm ../alloy-gem5/src/mem/protocol/SConsopts
-rm ../alloy-gem5/src/mem/SConscript
-rm ../alloy-gem5/src/cpu/SConscript
-
-# Copy new source files into alloy-gem5
-cp -r . ../alloy-gem5/
-
